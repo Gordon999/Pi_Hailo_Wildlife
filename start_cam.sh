04fd50fbaccf9edbe29b093a1364d1c6250a5c01
Gordon999 <Gordon999@users.noreply.github.com>
2024-07-26 13:17:49 +0100

    Update start_cam.sh

--- start_cam.sh
+++ start_cam.sh
@@ -2,4 +2,4 @@
 cd /home/gt64bw/hailo-rpi5-examples
 source setup_env.sh
 sleep 2
-/usr/bin/python /home/gt64bw/hailo-rpi5-examples/basic_pipelines/detection_008x5.py --input rpi -u
+/usr/bin/python /home/gt64bw/hailo-rpi5-examples/basic_pipelines/detection_001.py --input rpi -u
